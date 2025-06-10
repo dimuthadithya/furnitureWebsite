eb6b3c76ae4135f029879d5e7092de38ed274342
dimuthx353 <dimuthx353@gmail.com>
2025-06-10 17:32:02 +0530

    Enhance product card and details page: add styling for product images and links, implement product details layout with related products section.

--- includes/product-card.php
+++ includes/product-card.php
@@ -4,17 +4,23 @@ if (!isset($product)) {
     return;
 }
 ?>
-<div class="featured-product-card"> <?php if ($product['image_url']): ?>
-        <img
-            src="./<?php echo htmlspecialchars($product['image_url']); ?>"
-            alt="<?php echo htmlspecialchars($product['name']); ?>"
-            class="featured-product-image"
-            onerror="this.onerror=null; this.src='./assets/img/products/placeholder.jpg';" />
-    <?php else: ?>
-        <div class="featured-product-image bg-secondary"></div>
-    <?php endif; ?>
+<div class="featured-product-card"> <a href="product-details.php?id=<?php echo $product['product_id']; ?>" class="product-image-link">
+        <?php if ($product['image_url']): ?>
+            <img
+                src="./<?php echo htmlspecialchars($product['image_url']); ?>"
+                alt="<?php echo htmlspecialchars($product['name']); ?>"
+                class="featured-product-image"
+                onerror="this.onerror=null; this.src='./assets/img/products/placeholder.jpg';" />
+        <?php else: ?>
+            <div class="featured-product-image bg-secondary"></div>
+        <?php endif; ?>
+    </a>
 
-    <h3 class="featured-product-title"><?php echo htmlspecialchars($product['name']); ?></h3>
+    <h3 class="featured-product-title">
+        <a href="product-details.php?id=<?php echo $product['product_id']; ?>" class="product-title-link">
+            <?php echo htmlspecialchars($product['name']); ?>
+        </a>
+    </h3>
     <p class="featured-product-category"><?php echo htmlspecialchars($product['category_name']); ?></p>
     <div class="featured-product-description">
         <?php echo htmlspecialchars($product['description']); ?>
